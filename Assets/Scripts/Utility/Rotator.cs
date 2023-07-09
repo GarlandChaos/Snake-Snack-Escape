67eb2c5de974c0b4d6ad16e8b9d7473276dcffbb
Vini <vinimakegames@gmail.com>
2023-07-09 00:24:43 -0300

    Add key model

---
+++ Assets/Scripts/Utility/Rotator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 speed;
+
+    private void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+    }
+}
